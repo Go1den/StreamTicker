4920cbe1d4bc5e1316b304c8673ceee36c1ce885
Golden <gooneden@gmail.com>
2020-01-14 01:58:34 -0600

    Optimized style

--- settings.py
+++ settings.py
@@ -1,38 +1,37 @@
-
-#This is the duration in seconds of each message
+# This is the duration in seconds of each message
 MESSAGE_DURATION = 5
 
-#This is the duration in seconds of the pause between the previous message disappearing and the next message appearing
+# This is the duration in seconds of the pause between the previous message disappearing and the next message appearing
 MESSAGE_INTERMISSION = .5
 
-#This is the default color of the text. Plain color words are accepted.
+# This is the default color of the text. Plain color words are accepted.
 MESSAGE_COLOR = 'white'
 
-#Allows a modifier on the font, such as bold or italic.
+# Allows a modifier on the font, such as bold or italic.
 MESSAGE_STYLE = 'bold'
 
-#This is the name of the font. STRONG PREFERENCE FOR MONOSPACED FONTS. If it can't find the font you list here, it will attempt to use a (bad) default.
+# This is the name of the font. STRONG PREFERENCE FOR MONOSPACED FONTS. If it can't find the font you list here, it will attempt to use a (bad) default.
 MESSAGE_FONT_FACE = 'PT Mono'
 
-#This is the maximum number of characters in a message before it will attempt to shrink the font. If you set this too high, your messages could trail outside of the window
+# This is the maximum number of characters in a message before it will attempt to shrink the font. If you set this too high, your messages could trail outside of the window
 MAX_LENGTH_FOR_NORMAL_FONT_SIZE = 16
 
-#This is the normal font size of a message
+# This is the normal font size of a message
 NORMAL_FONT_SIZE = 26
 
-#This is the smaller font size used when a message exceeds the length of MAX_LENGTH_FOR_NORMAL_FONT_SIZE
+# This is the smaller font size used when a message exceeds the length of MAX_LENGTH_FOR_NORMAL_FONT_SIZE
 SMALLER_FONT_SIZE = 22
 
-#Width of the window in pixels
+# Width of the window in pixels
 WINDOW_WIDTH = 400
 
-#Height of the window in pixels
+# Height of the window in pixels
 WINDOW_HEIGHT = 44
 
-#Default window background color. Plain color words are accepted.
+# Default window background color. Plain color words are accepted.
 WINDOW_BG_COLOR = 'black'
 
-#This is the location of the background image. If no image is provided or the filepath is bad, the background color above will be used.
+# This is the location of the background image. If no image is provided or the filepath is bad, the background color above will be used.
 WINDOW_BG_IMAGE = "imagefiles/background.png"
 
 MOVE_ALL_ON_LINE_DELAY = .004
