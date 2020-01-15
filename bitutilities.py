235951023a170c39ba08f64de99d5dfd3f8cd75d
Go1den <gooneden@gmail.com>
2020-01-14 21:32:13 -0600

    Cleaned up image files

--- bitutilities.py
+++ bitutilities.py
@@ -1,13 +1,13 @@
 def chooseBitIcon(bits):
     if bits > 99999:
-        return ["bitimagefiles/bit100000reduced.png"]
+        return ["bitimagefiles/bit100000.png"]
     elif bits > 9999:
-        return ["bitimagefiles/bit10000reduced.png"]
+        return ["bitimagefiles/bit10000.png"]
     elif bits > 4999:
-        return ["bitimagefiles/bit5000reduced.png"]
+        return ["bitimagefiles/bit5000.png"]
     elif bits > 999:
-        return ["bitimagefiles/bit1000reduced.png"]
+        return ["bitimagefiles/bit1000.png"]
     elif bits > 99:
-        return ["bitimagefiles/bit100reduced.png"]
+        return ["bitimagefiles/bit100.png"]
     else:
-        return ["bitimagefiles/bit10reduced.png"]
+        return ["bitimagefiles/bit10.png"]
