59a55d2538d59638821233abb7014c0fb8fbe86c
Golden <gooneden@gmail.com>
2020-01-17 21:32:40 -0600

    more messing with gui

--- src/slideGUI.py
+++ src/slideGUI.py
@@ -6,27 +6,38 @@ def addNewSlide():
 def previewMessage():
     preview.configure(text=prefix.get() + text.get() + suffix.get())
 
+def testReadingFile():
+    with open(filepath.get()) as f:
+        filecontents.configure(text=f.read())
+
 master = tk.Tk()
 tk.Label(master, text="Message Prefix").grid(row=0, sticky=tk.E)
 tk.Label(master, text="Message Text").grid(row=1, sticky=tk.E)
 tk.Label(master, text="Message Suffix").grid(row=2, sticky=tk.E)
 tk.Label(master, text="Use Twitch Bit icons").grid(row=3, sticky=tk.E)
 tk.Label(master, text="Preview: ").grid(row=4, sticky=tk.E)
+tk.Label(master, text="File path:").grid(row=6, sticky=tk.E)
 
 prefix = tk.Entry(master)
 text = tk.Entry(master)
 suffix = tk.Entry(master)
 preview = tk.Label(master)
 bits = tk.Checkbutton(master)
+filepath = tk.Entry(master)
+filecontents = tk.Label(master)
+
 prefix.insert(0, "")
 text.insert(0, "")
 suffix.insert(0, "")
+filepath.insert(0, "")
 
 prefix.grid(row=0, column=1)
 text.grid(row=1, column=1)
 suffix.grid(row=2, column=1)
 bits.grid(row=3, column=1)
 preview.grid(row=4, column=1)
+filepath.grid(row=6, column=1)
+filecontents.grid(row=7, column=1)
 
 tk.Button(master,
           text='Quit',
@@ -44,6 +55,13 @@ tk.Button(master,
                                        column=2,
                                        sticky=tk.W,
                                        pady=4)
+
+tk.Button(master,
+          text='Test reading file',
+          command=testReadingFile).grid(row=8,
+                                        column=0,
+                                        sticky=tk.W,
+                                        pady=4)
 
 master.mainloop()
 tk.mainloop()
