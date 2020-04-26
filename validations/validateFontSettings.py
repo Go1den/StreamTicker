e0264fcfe03ecd4a270718a95e15d0da92d44b1d
Go1den <gooneden@gmail.com>
2020-04-26 16:35:25 -0500

    Removed outdated font validations

--- validations/validateFontSettings.py
+++ validations/validateFontSettings.py
@@ -4,14 +4,5 @@ def validate(fields):
     if not fields.VAR_ENTRY_NORMAL_FONT_SIZE.get().isnumeric() or int(fields.VAR_ENTRY_NORMAL_FONT_SIZE.get()) < 8 or int(fields.VAR_ENTRY_NORMAL_FONT_SIZE.get()) > 256:
         messagebox.showinfo("Error", "Normal message font size must be a whole number between 8 and 256.")
         return False
-    elif not fields.VAR_ENTRY_SMALLER_FONT_SIZE.get().isnumeric() or int(fields.VAR_ENTRY_SMALLER_FONT_SIZE.get()) < 8 or int(fields.VAR_ENTRY_SMALLER_FONT_SIZE.get()) > 256:
-        messagebox.showinfo("Error", "Long message font size must be a whole number between 8 and 256.")
-        return False
-    elif int(fields.VAR_ENTRY_NORMAL_FONT_SIZE.get()) < int(fields.VAR_ENTRY_SMALLER_FONT_SIZE.get()):
-        messagebox.showinfo("Error", "Normal message font size cannot be less than long message font size.")
-        return False
-    elif not fields.VAR_ENTRY_MAX_LENGTH_FOR_NORMAL_FONT_SIZE.get().isnumeric() or int(fields.VAR_ENTRY_MAX_LENGTH_FOR_NORMAL_FONT_SIZE.get()) < 1:
-        messagebox.showinfo("Error", "Max length for normal messages must be a positive whole number.")
-        return False
     # add validation on message style whenever that gets added to the settings menu
     return True
