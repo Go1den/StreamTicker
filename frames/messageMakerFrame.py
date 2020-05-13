5b040f194f9f6154e3173d06eee17803deb04317
Go1den <gooneden@gmail.com>
2020-05-13 00:51:32 -0500

    More work in progress on the message maker window

--- frames/messageMakerFrame.py
+++ frames/messageMakerFrame.py
@@ -1,32 +1,57 @@
-from tkinter import Frame, GROOVE, Label, Entry, E, W, Button
+from tkinter import Frame, GROOVE, Label, Entry, E, W, Button, StringVar
 
 class MessageMakerFrame:
     def __init__(self, master, existingMessage):
         self.frame = Frame(master, bd=2, relief=GROOVE)
+        self.message = existingMessage
+        print("Self.message is")
+        print(self.message)
+
+        self.nickname = StringVar()
+        self.prefix = StringVar()
+        self.file = StringVar()
+        self.suffix = StringVar()
+        self.image = StringVar()
+
+        self.populateFieldsOnLoad()
 
         labelMessageSettings = Label(self.frame, text="Message Settings:")
-        labelMessageSettings.grid(row=0, column=0, sticky=W)
+        labelMessageSettings.grid(row=0, column=0, sticky=W, padx=1, pady=1)
 
         labelNickname = Label(self.frame, text="Nickname this message:")
-        labelNickname.grid(row=1, column=0, sticky=E)
+        labelNickname.grid(row=1, column=0, sticky=E, padx=1, pady=1)
 
-        entryNickname = Entry(self.frame)
-        entryNickname.grid(row=1, column=1, sticky=W)
+        entryNickname = Entry(self.frame, textvariable=self.nickname, width=30)
+        entryNickname.grid(row=1, column=1, sticky=W, padx=1, pady=1)
 
         labelPrefix = Label(self.frame, text="Message Prefix:")
-        labelPrefix.grid(row=2, column=0, sticky=E)
+        labelPrefix.grid(row=2, column=0, sticky=E, padx=1, pady=1)
 
-        entryPrefix = Entry(self.frame)
-        entryPrefix.grid(row=2, column=1, sticky=W)
+        entryPrefix = Entry(self.frame, textvariable=self.prefix, width=30)
+        entryPrefix.grid(row=2, column=1, sticky=W, padx=1, pady=1)
 
         buttonTextFile = Button(self.frame, text="(Optional) Select Text File")
-        buttonTextFile.grid(row=3, column=0, sticky=E)
+        buttonTextFile.grid(row=3, column=0, sticky=E, padx=1, pady=1)
 
-        entryTextFile = Entry(self.frame)
-        entryTextFile.grid(row=3, column=1, sticky=W)
+        labelTextFile = Label(self.frame, textvariable=self.file)
+        labelTextFile.grid(row=3, column=1, sticky=W, padx=1, pady=1)
 
         labelSuffix = Label(self.frame, text="Message Suffix:")
-        labelSuffix.grid(row=4, column=0, sticky=E)
+        labelSuffix.grid(row=4, column=0, sticky=E, padx=1, pady=1)
 
-        entrySuffix = Entry(self.frame)
-        entrySuffix.grid(row=4, column=1, sticky=W)
+        entrySuffix = Entry(self.frame, textvariable=self.suffix, width=30)
+        entrySuffix.grid(row=4, column=1, sticky=W, padx=1, pady=1)
+
+        buttonImageFile = Button(self.frame, text="(Optional) Select Image File")
+        buttonImageFile.grid(row=5, column=0, sticky=E, padx=1, pady=1)
+
+        labelImageFile = Label(self.frame, textvariable=self.image)
+        labelImageFile.grid(row=5, column=1, sticky=W, padx=1, pady=1)
+
+    def populateFieldsOnLoad(self):
+        if self.message:
+            self.nickname.set(self.message.get("nickname"))
+            self.prefix.set(self.message.get("prefixText"))
+            self.file.set(self.message.get("filePath"))
+            self.suffix.set(self.message.get("suffixText"))
+            self.image.set(self.message.get("image"))
--- messageMakerWindow.py
+++ messageMakerWindow.py
@@ -1,6 +1,9 @@
-from tkinter import Toplevel
+from tkinter import Toplevel, E
 
+from frames.messageFrame import MessageFrame
 from frames.messageMakerFrame import MessageMakerFrame
+from frames.messageMakerOkCancelFrame import MessageMakerOkCancelFrame
+from globalMessageSettings import GlobalMessageSettings
 
 class MessageMakerWindow:
     def __init__(self, parent, existingMessage):
@@ -16,8 +19,17 @@ class MessageMakerWindow:
         self.master.grab_set()
         self.master.protocol("WM_DELETE_WINDOW", self.deleteWindow)
 
-        mmFrame = MessageMakerFrame(self.master, existingMessage)
-        mmFrame.frame.grid(row=0, column=0)
+        self.fields = GlobalMessageSettings()
+        self.setGlobalMessageSettings()
+
+        self.mmFrame = MessageMakerFrame(self.master, existingMessage)
+        self.mmFrame.frame.grid(row=0, column=0, padx=(4, 4), pady=4)
+
+        self.globalMessageSettingsFrame = MessageFrame(self.master, self.fields)
+        self.globalMessageSettingsFrame.frame.grid(row=1, column=0, padx=(4, 4), pady=4)
+
+        self.okCancelFrame = MessageMakerOkCancelFrame(self)
+        self.okCancelFrame.frame.grid(row=2, column=0, padx=4, pady=4, sticky=E)
 
         self.master.deiconify()
         self.master.mainloop()
@@ -27,3 +39,12 @@ class MessageMakerWindow:
         self.parent.lift()
         self.parent.wm_attributes("-topmost", 1)
         self.parent.grab_set()
+
+    def setGlobalMessageSettings(self):
+        self.fields.VAR_ENTRY_MESSAGE_DURATION.set("")
+        self.fields.VAR_ENTRY_MESSAGE_INTERMISSION.set("")
+        self.fields.VAR_FONT_COMBO_BOX.set("")
+        self.fields.VAR_LABEL_MESSAGE_COLOR_FOREGROUND.set("")
+        self.fields.VAR_LABEL_MESSAGE_COLOR_TEXT.set("")
+        self.fields.VAR_ENTRY_NORMAL_FONT_SIZE.set("")
+        self.fields.VAR_ENTRY_MOVE_ALL_ON_LINE_DELAY.set("")
