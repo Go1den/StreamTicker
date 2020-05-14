a53c35ea9979d9da926dec908f59ea0695c08aa1
Go1den <gooneden@gmail.com>
2020-05-13 23:04:04 -0500

    Changed wording on font error

--- validations/validateFontSettings.py
+++ validations/validateFontSettings.py
@@ -2,7 +2,7 @@ from tkinter import messagebox
 
 def validate(fields):
     if not fields.VAR_ENTRY_NORMAL_FONT_SIZE.get().isnumeric() or int(fields.VAR_ENTRY_NORMAL_FONT_SIZE.get()) < 8 or int(fields.VAR_ENTRY_NORMAL_FONT_SIZE.get()) > 256:
-        messagebox.showinfo("Error", "Normal message font size must be a whole number between 8 and 256.")
+        messagebox.showinfo("Error", "Font size must be a whole number between 8 and 256.")
         return False
     # add validation on message style whenever that gets added to the settings menu
     return True
