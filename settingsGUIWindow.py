f4d2a70b3b9db5052aea63efbf8f9cd73da02fea
Go1den <gooneden@gmail.com>
2020-05-13 22:36:48 -0500

    SettingsGUIWindow updated to remove departure and arrival frames, as they are pointless now

--- settingsGUIWindow.py
+++ settingsGUIWindow.py
@@ -1,8 +1,6 @@
-from tkinter import Toplevel, NSEW, E, Frame, GROOVE
+from tkinter import Toplevel, NSEW, E
 
-from frames.arrivalFrame import ArrivalFrame
 from frames.backgroundFrame import BackgroundFrame
-from frames.departureFrame import DepartureFrame
 from frames.messageFrame import MessageFrame
 from frames.okCancelFrame import OkCancelFrame
 from frames.slidingFrame import SlidingFrame
@@ -22,27 +20,17 @@ class SettingsGUIWindow:
 
         self.fields = SettingsGUIFields()
 
-        self.transitionFrame = Frame(self.master, bd=2, relief=GROOVE)
-
-        self.dFrame = DepartureFrame(self.transitionFrame, self.fields)
-        self.dFrame.frame.grid(row=0, column=0, sticky=NSEW)
-
-        self.aFrame = ArrivalFrame(self.transitionFrame, self.fields)
-        self.aFrame.frame.grid(row=0, column=1, sticky=NSEW)
-
-        self.transitionFrame.grid(row=0, column=0, columnspan=2, sticky=NSEW, padx=4, pady=4)
-
         self.mFrame = MessageFrame(self.master, self.fields)
-        self.mFrame.frame.grid(row=0, column=2, sticky=NSEW, padx=4, pady=4)
+        self.mFrame.frame.grid(row=0, column=0, sticky=NSEW, padx=4, pady=4)
 
         self.bgFrame = BackgroundFrame(self.master, self.fields)
-        self.bgFrame.frame.grid(row=0, column=3, sticky=NSEW, padx=4, pady=4)
+        self.bgFrame.frame.grid(row=0, column=1, sticky=NSEW, padx=4, pady=4)
 
         self.sFrame = SlidingFrame(self.master, self.fields)
-        self.sFrame.frame.grid(row=1, column=0, columnspan=4, sticky=NSEW, padx=4, pady=4)
+        self.sFrame.frame.grid(row=1, column=0, columnspan=2, sticky=NSEW, padx=4, pady=4)
 
         self.okFrame = OkCancelFrame(self.master, self.fields)
-        self.okFrame.frame.grid(row=2, column=3, sticky=E, padx=4, pady=4)
+        self.okFrame.frame.grid(row=2, column=1, sticky=E, padx=4, pady=4)
 
         self.master.deiconify()
         self.master.mainloop()
