c26d3283ff07bbfaf4880f115b6fa788a122b1aa
Go1den <gooneden@gmail.com>
2020-05-13 22:35:04 -0500

    SlidingFrame is now only one column

--- frames/slidingFrame.py
+++ frames/slidingFrame.py
@@ -8,30 +8,30 @@ class SlidingFrame:
         self.frame = Frame(master, bd=2, relief=GROOVE)
 
         self.LABEL_WINDOW_SETTINGS = Label(self.frame, text="Window Settings")
-        self.LABEL_WINDOW_SETTINGS.grid(row=0, column=0, sticky=W)
+        self.LABEL_WINDOW_SETTINGS.grid(row=0, sticky=W)
         self.SCALE_WINDOW_WIDTH = Scale(self.frame, variable=fields.VAR_ENTRY_WINDOW_WIDTH, from_=400, to=1920, tickinterval=1520, orient=HORIZONTAL, resolution=4, length=SCALE_LENGTH,
                                         command=lambda x: self.updateXScales(), label="Window Width in Pixels:")
-        self.SCALE_WINDOW_WIDTH.grid(row=1, column=0)
+        self.SCALE_WINDOW_WIDTH.grid(row=1)
 
         self.SCALE_IMAGE_X_POS = Scale(self.frame, variable=fields.VAR_ENTRY_IMAGE_X_POS, from_=0, to=400, tickinterval=400, orient=HORIZONTAL, resolution=1, length=SCALE_LENGTH,
                                        label="Image X Coordinate (location of image center)")
-        self.SCALE_IMAGE_X_POS.grid(row=1, column=1)
+        self.SCALE_IMAGE_X_POS.grid(row=4)
 
         self.SCALE_MESSAGE_X_POS = Scale(self.frame, variable=fields.VAR_ENTRY_MESSAGE_X_POS, from_=0, to=400, tickinterval=40, orient=HORIZONTAL, resolution=1, length=SCALE_LENGTH,
                                          label="Message X Coordinate (location of message start)")
-        self.SCALE_MESSAGE_X_POS.grid(row=2, column=0)
+        self.SCALE_MESSAGE_X_POS.grid(row=2)
 
         self.SCALE_BG_X_POS = Scale(self.frame, variable=fields.VAR_ENTRY_BACKGROUND_X_POS, from_=0, to=400, tickinterval=400, orient=HORIZONTAL, resolution=1, length=SCALE_LENGTH,
                                     label="Background X Coordinate (location of BG center)")
-        self.SCALE_BG_X_POS.grid(row=2, column=1)
+        self.SCALE_BG_X_POS.grid(row=5)
 
         self.SCALE_WINDOW_HEIGHT = Scale(self.frame, variable=fields.VAR_ENTRY_WINDOW_HEIGHT, from_=40, to=1080, tickinterval=1040, orient=HORIZONTAL, resolution=4, length=SCALE_LENGTH,
                                          command=lambda x: self.updateYScales(), label="Window Height in Pixels:")
-        self.SCALE_WINDOW_HEIGHT.grid(row=3, column=0)
+        self.SCALE_WINDOW_HEIGHT.grid(row=3)
 
         self.SCALE_BG_Y_POS = Scale(self.frame, variable=fields.VAR_ENTRY_BACKGROUND_Y_POS, from_=0, to=40, tickinterval=40, orient=HORIZONTAL, resolution=1, length=SCALE_LENGTH,
                                     label="Background Y Coordinate (location of BG center)")
-        self.SCALE_BG_Y_POS.grid(row=3, column=1)
+        self.SCALE_BG_Y_POS.grid(row=6)
 
     def updateXScales(self):
         self.SCALE_IMAGE_X_POS.configure(to=self.SCALE_WINDOW_WIDTH.get(), tickinterval=self.SCALE_WINDOW_WIDTH.get())
