f11e7fc40d3ec18cd48f659b02e0436502df1f49
Go1den <gooneden@gmail.com>
2020-05-21 00:38:01 -0500

    Created a defaultSettings file to retrieve from when user clicks default settings in right click menu

---
+++ defaultSettings.py
@@ -0,0 +1,25 @@
+def getDefaultSettings():
+    return {
+        "window": {
+            "MOVE_ALL_ON_LINE_DELAY": "Fast",
+            "WINDOW_BG_IMAGE": "imagefiles/background1.png",
+            "WINDOW_WIDTH": "400",
+            "BACKGROUND_X_POS": "200",
+            "WINDOW_HEIGHT": "40",
+            "BACKGROUND_Y_POS": "20",
+            "IMAGE_X_POS": "20",
+            "WINDOW_BG_COLOR": "#000000",
+            "MESSAGE_X_POS": "60"
+        },
+        "message": {
+            "MESSAGE_STYLE": "",
+            "MESSAGE_COLOR": "#ffffff",
+            "DEFAULT_IMAGE": "imagefiles/stLogo28.png",
+            "MESSAGE_FONT_FACE": "Courier New",
+            "MESSAGE_INTERMISSION": ".5",
+            "NORMAL_FONT_SIZE": "26",
+            "MESSAGE_DURATION": "5",
+            "ARRIVAL": "Pick For Me",
+            "DEPARTURE": "Pick For Me"
+        }
+    }
