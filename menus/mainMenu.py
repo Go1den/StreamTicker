e5d446e3c03f5631daaf626ce20ceb6a90683baf
Go1den <gooneden@gmail.com>
2021-05-04 17:53:03 -0500

    Added right click menu option to view release notes

--- menus/mainMenu.py
+++ menus/mainMenu.py
@@ -32,6 +32,7 @@ def getMainMenu(win, e):
     rmenu.add_checkbutton(label="  Always on Top", variable=win.alwaysOnTop, onvalue=True, offvalue=False, command=lambda: win.updateAlwaysOnTop())
     rmenu.add_separator()
     rmenu.add_cascade(label="  Report Issue", menu=issueMenu)
+    rmenu.add_command(label="  Release Notes", command=lambda: webbrowser.open('https://pastebin.com/wJpGZE0F', new=2))
     rmenu.add_command(label='  Check for Updates', command=lambda: win.checkForUpdates())
     rmenu.add_separator()
     rmenu.add_command(label='  About', command=lambda: AboutWindow(win))
