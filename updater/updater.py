0339f036079a4573175489da8d5317a1fb505d62
Go1den <gooneden@gmail.com>
2021-05-21 19:46:38 -0500

    Implemented auto-updater

--- updater/updater.py
+++ updater/updater.py
@@ -1,7 +1,9 @@
 from json import load
-from os import getcwd, walk, remove, makedirs
+from logging import info, basicConfig, INFO
+from os import getcwd, walk, remove, makedirs, chdir
 from os.path import abspath, join, exists, samefile
 from shutil import copy, rmtree
+from subprocess import Popen
 from time import sleep
 from urllib.request import urlretrieve
 from zipfile import ZipFile
@@ -10,17 +12,22 @@ def getSettingsJSONOnStartup():
     with open("updaterSettings.cfg", "r") as f:
         return load(f)
 
+def logAndPrint(s: str):
+    info(s)
+    print(s)
+
 try:
-    print("StreamTicker update in progress. Please wait.")
-    print("Loading updater settings")
+    basicConfig(filename="log.txt", encoding="utf-8", level=INFO, format='%(asctime)s: %(message)s')
+    logAndPrint("StreamTicker update in progress. Please wait.")
+    logAndPrint("Loading updater settings")
     settings = getSettingsJSONOnStartup()
-    print("Updater settings loaded")
-    print("Downloading latest version...")
+    logAndPrint("Updater settings loaded")
+    logAndPrint("Downloading latest version...")
     urlretrieve(settings["downloadURL"], getcwd() + settings["temporaryZip"])
-    print("Download complete. Unzipping...")
+    logAndPrint("Download complete. Unzipping...")
     with ZipFile(getcwd() + settings["temporaryZip"], 'r') as zip_ref:
         zip_ref.extractall(getcwd() + settings["temporaryExtractFolder"])
-    print("Unzip complete. Calculating files to be updated...")
+    logAndPrint("Unzip complete. Calculating files to be updated...")
 
     root_src_dir = getcwd() + settings["temporaryExtractFolder"]
     root_dst_dir = settings["destinationDirectory"]
@@ -30,56 +37,60 @@ try:
     for src_dir, dirs, files in walk(root_src_dir):
         for file_ in files:
             fileCount += 1
-    print("Updating " + str(fileCount) + " files")
+    logAndPrint("Updating " + str(fileCount) + " files")
 
     filesUpdated = 0
     for src_dir, dirs, files in walk(root_src_dir):
         dst_dir = src_dir.replace(root_src_dir, root_dst_dir, 1)
         if not exists(dst_dir):
             makedirs(dst_dir)
-            print("Added new folder: " + str(dst_dir))
+            logAndPrint("Added new folder: " + str(dst_dir))
         for file_ in files:
             filesUpdated += 1
-            print("Updating file " + str(filesUpdated) + " of " + str(fileCount))
+            logAndPrint("Updating file " + str(filesUpdated) + " of " + str(fileCount))
             src_file = join(src_dir, file_)
             dst_file = join(dst_dir, file_)
             if file_ in settings["doNotUpdateFiles"] and exists(dst_file):
-                print("This file is in the updater's Do Not Update list and won't be updated: " + str(file_))
+                logAndPrint("This file is in the updater's Do Not Update list and won't be updated: " + str(file_))
             else:
                 if exists(dst_file):
                     if samefile(src_file, dst_file):
-                        print("Same file found: " + str(file_))
+                        logAndPrint("Same file found: " + str(file_))
                         continue
                     remove(dst_file)
-                    print("Removed old file: " + str(file_))
+                    logAndPrint("Removed old file: " + str(file_))
                 copy(src_file, dst_dir)
-                print("Copied new file: " + str(file_))
+                logAndPrint("Copied new file: " + str(file_))
 
     for dst_dir, dirs, files in walk(root_dst_dir):
         directory = abspath(dst_dir).replace(abspath(root_dst_dir), "").replace("\\", "", 1)
         if directory and any(directory.startswith(exclude) for exclude in settings["doNotCleanupDirectories"]):
-            print("Skipping cleanup on directory: " + directory)
+            logAndPrint("Skipping cleanup on directory: " + directory)
             continue
         src_dir = dst_dir.replace(root_dst_dir, root_src_dir, 1)
         if not exists(src_dir):
             rmtree(dst_dir)
-            print("Cleanup detected an unknown folder and removed it: " + str(dst_dir))
+            logAndPrint("Cleanup detected an unknown folder and removed it: " + str(dst_dir))
             continue
         for file_ in files:
             src_file = join(src_dir, file_)
             dst_file = join(dst_dir, file_)
             if not exists(src_file) and not any(file_.endswith(extension) for extension in settings["doNotCleanupFileExtensions"]):
                 remove(dst_file)
-                print("Cleanup detected an unknown file and removed it: " + str(file_))
+                logAndPrint("Cleanup detected an unknown file and removed it: " + str(file_))
 
     remove(getcwd() + settings["temporaryZip"])
     rmtree(getcwd() + settings["temporaryExtractFolder"])
-    print("The updater has finished. You may now close the window. This window will automatically close in 10 minutes.")
-    sleep(600)
+    logAndPrint("Update complete. Attempting to restart StreamTicker...")
+    sleep(3)
+    chdir(settings["destinationDirectory"])
+    Popen(settings["restartExecutable"])
+    exit(0)
 except Exception as e:
-    print("The updater encountered an error and will now close.")
-    print("Error information:")
-    print(str(e))
-    print("You may now close the window or copy this information for your records. This window will automatically close in 10 minutes.")
+    logAndPrint("The updater encountered an error and will now close.")
+    logAndPrint("Error information:")
+    logAndPrint(str(e))
+    logAndPrint("You may need to manually download StreamTicker again to ensure all needed files are present.")
+    logAndPrint("Information about this update failure was stored in streamticker/updater/log.txt. This window will automatically close in 10 minutes.")
     sleep(600)
-
+    exit(1)
--- windows/streamTickerWindow.py
+++ windows/streamTickerWindow.py
@@ -1,5 +1,6 @@
 import json
 import os
+import subprocess
 import sys
 import time
 import urllib.request
@@ -271,12 +272,17 @@ class StreamTickerWindow(Tk):
         currentVersion = "2.0.8"
         try:
             f = urllib.request.urlopen('https://www.go1den.com/streamtickerversion/version.txt')
-            if not currentVersion == str(f.read().decode()):
-                messagebox.showinfo("New Version Available",
-                                    "A new version of StreamTicker is available. To update, open your StreamTicker directory, navigate to the updater folder, and run updater.exe, or you can download it manually from the website.",
-                                    parent=self)
+            if not currentVersion == str(f.read().decode()) and messagebox.askyesno("New Version Available",
+                                                                                    "A new version of StreamTicker is available. Would you like to update now?", parent=self):
+                self.updateStreamTicker()
         except HTTPError:
             messagebox.showerror("Error", "Failed to update. You may need to download the new version manually.", parent=self)
+
+    def updateStreamTicker(self):
+        # There's very little chance this is compatible with Mac or Linux
+        os.chdir(os.getcwd() + "/updater")
+        subprocess.Popen("start cmd /C updater.exe", shell=True)
+        exit(0)
 
     def loadDefaultSettings(self):
         if messagebox.askokcancel("Restore Default Settings", "Are you sure you want to restore the default settings for StreamTicker?", parent=self):
